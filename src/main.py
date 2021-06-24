39d9b151f16886a3fc14530d94f2861987b03808
DolicaAkelloEgwel <dolica.akello-egwel@stfc.ac.uk>
2021-06-24 14:26:03 +0100

    file dialogs

--- src/main.py
+++ src/main.py
@@ -17,8 +17,25 @@ class MainWindow(QMainWindow):
         file1LineEdit = self.ui.file1LineEdit
         file2LineEdit = self.ui.file2LineEdit
 
+        file1PushButton.clicked.connect(lambda: self._open_file_dialog(file1LineEdit))
+        file2PushButton.clicked.connect(lambda: self._open_file_dialog(file2LineEdit))
+
     def _open_file_dialog(self, line_edit):
-        filename = QFileDialog.getOpenFileName(self, tr("Open File"), "~", tr(""))
+        filename = QFileDialog.getOpenFileName(self, "Open File", "", "Data (*.csv *.tsv *.txt *.xlsx)")[0]
+        line_edit.setText(filename)
+
+    def run_csvlink(self):
+        from . import csvhelpers
+        import dedupe
+
+        data_1 = csvhelpers.readData(self.filename1, self.field_names_1,
+                                    delimiter=",",
+                                    prefix='input_1')
+        data_2 = csvhelpers.readData(self.filename2, self.field_names_2,
+                                    delimiter=",",
+                                    prefix='input_2')
+
+
 
 if __name__ == "__main__":
     app = QApplication([])
